d1dd228261a6681d7356aa4986e36beea1f41111
Gusti <augustincalin@yahoo.com>
2016-10-07 23:09:57 +0200

    adds the image

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Checkout branches to see the progress.
 
 This step adds the image to bundle.
+1. npm i copy-webpack-plugin -D
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var  CopyWebpackPlugin  =  require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./src/app/main.js",
@@ -6,16 +7,25 @@ module.exports = {
         path: "dist",
         filename: "bundle.js"
     },
-    module:{
+    module: {
         loaders: [
-            {test:/\.css$/, loader:'style!css'},
-            {test:/\.png$/, loader:'file'}
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.png$/, loader: 'file' },
+            { test: /\.jpg$/, loader: 'file?name=[path][name].[ext]' }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
+        }),
+        new CopyWebpackPlugin(
+            [
+                {
+                    from: 'src/assets/images/frankfurt.jpg',
+                    to: 'assets/images/frankfurt.jpg'
+                }
+            ]
+        )
     ]
 
 };
